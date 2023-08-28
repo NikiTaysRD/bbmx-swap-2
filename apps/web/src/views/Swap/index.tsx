aa04634c54fcad4d2a7e6c9c19a6eb20a8d743c7
poznianski <viacheslav.miasoiedov@redduck.io>
2023-08-28 18:16:05 +0300

    feat: almost finished header for swap

--- apps/web/src/views/Swap/index.tsx
+++ apps/web/src/views/Swap/index.tsx
@@ -69,8 +69,6 @@ export default function Swap() {
     [inputCurrencyId, outputCurrencyId, onCurrencySelection, warningSwapHandler],
   )
 
-  const { isDark } = useTheme()
-
   return (
     <Page removePadding={isChartExpanded} hideFooterOnDesktop={isChartExpanded}>
       <Flex width={['328px', '100%']} height="100%" justifyContent="center" position="relative" alignItems="flex-start">
